e1f120304c53a1f351e7d31a801cd318cd91bf51
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2022-12-10 21:14:58 -0500

    fixed bugs

--- visualizations.py
+++ visualizations.py
@@ -2,20 +2,19 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
-from quantile_dotplot import ntile_dotplot
+from dotplot import ntile_dotplot
 from scipy import stats
-from matplotlib.colors import LinearSegmentedColormap
-import matplotlib.patches as patches
 
 
 # globals
 N_PLOTS = 3
-N_MINUTES = 30
+N_MINUTES = 18
+
 
 data = [
-    np.random.lognormal(mean=np.log(12), sigma=0.2, size=1_000_000),
-    np.random.normal(loc=14, scale=0.5, size=1_000_000),
-    np.random.normal(loc=16, scale=1.5, size=1_000_000)
+    np.random.gamma(1.5, 2.0, size=1_000_000),
+    np.random.gamma(7.0, 1.0, size=1_000_000),
+    np.random.gamma(25.0, 0.5, size=1_000_000),
     ]
 xs = np.linspace(0, N_MINUTES, 200)
 
@@ -26,74 +25,69 @@ colors = ["blue", "pink", "green"]
 fig, axs = None, None
 
 def quantilePlot():
-    plt.rcParams.update({'font.size': 25})
+    plt.rcParams.update({'font.size': 12})
     for i in range(N_PLOTS):
         axs[i] = ntile_dotplot(data[i], dots=20, facecolor=colors[i], linewidth=0, ax=axs[i])
 
 def pdfPlot():
-    plt.rcParams.update({'font.size': 10})
+    plt.rcParams.update({'font.size': 12})
     # PDFs specified in the form stats.lognorm([variance], mean) for lognormal and stats.norm([mean], variance) for normal
-    dists = [stats.lognorm([0.2], scale=12), stats.norm([14], scale=0.5), stats.norm([16], scale=1.5)]
+    dists = [stats.gamma(1.5, scale = 2.0), stats.gamma(7.0, scale = 1.0), stats.gamma(25.0,  scale = 0.5)]
     # lines corresponding to time
     for i in range(N_PLOTS):
         ax = axs[i]
         ax.plot(xs, dists[i].pdf(xs), color = colors[i])
 
 def greenRedPlot():
-    plt.rcParams.update({'font.size': 10})
+    plt.rcParams.update({'font.size': 12})
     for i in range(N_PLOTS):
-        cMap = []
         ax = axs[i]
-        colTuples = [(stats.percentileofscore(data[i], x)/100, 1 - stats.percentileofscore(data[i], x)/100, 0) for x in xs]
-        for value, colour in zip(xs, colTuples):
-            cMap.append((value/N_MINUTES, colour))
-        customColorMap = LinearSegmentedColormap.from_list("custom", cMap)
-        ax.imshow(
-            np.tile(np.linspace(0, 1, N_MINUTES + 1), (3, 1)),
-            cmap = customColorMap,
-            interpolation = 'bicubic',
-        )
+        for x in xs:
+            ax.set_ylim(bottom=0, top=1)
+            ax.scatter(x, 0.5, color=(stats.percentileofscore(data[i], x)/100, 1 - stats.percentileofscore(data[i], x)/100, 0), marker="s")
 
 # shuffled list of all the plot types
-timeTextElems = []
-riskTextElems = []
-lines = []
-
 def transformAxs(axs):
+    timeTextElems, riskTextElems, lines = [], [], []
     for i in range(N_PLOTS):
         ax = axs[i]
-        ax.set_xticks(np.linspace(0, N_MINUTES, 16))
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
         ax.set_xlim(left=0, right=N_MINUTES)
+        ax.set_xticks(np.linspace(0, N_MINUTES, 10))
         ax.get_yaxis().set_visible(False)
         lines.append(ax.axvline(x = times[i], color = "black"))
-        timeTextElems.append(ax.text(0.5, 1.5, f"{times[i]} mins", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes))
-        riskTextElems.append(ax.text(0.5, 1.2, f"{risks[i]}% risk", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes))
+        timeTextElems.append(ax.text(0.5, 1.5, f"{times[i]} mins", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, weight="bold"))
+        riskTextElems.append(ax.text(0.5, 1.2, f"{risks[i]}% risk", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, weight="bold"))
 
-        bottom, top = ax.get_ylim()
-        mid = (bottom + top)/2
+        _, top = ax.get_ylim()
         if i == 0:
-            ax.text(-3, mid, ' QYE ', color='blue', 
-                bbox=dict(facecolor='none', edgecolor='blue', boxstyle='round,pad=1', linewidth=2))
+            ax.text(0, top, ' QYE ', color='blue', 
+                bbox=dict(facecolor='none', edgecolor='blue', boxstyle='round,pad=0.5', linewidth=1))
         elif i == 1:
-            ax.text(-3, mid, '  AL  ', color='pink', 
-                bbox=dict(facecolor='none', edgecolor='pink', boxstyle='round,pad=1', linewidth=2))
+            ax.text(0, top, '  AL  ', color='pink', 
+                bbox=dict(facecolor='none', edgecolor='pink', boxstyle='round,pad=0.5', linewidth=1))
         else:
-            ax.text(-3, mid, '  CC  ', color='green', 
-                bbox=dict(facecolor='none', edgecolor='green', boxstyle='round,pad=1', linewidth=2))
+            ax.text(0, top, '  CC  ', color='green', 
+                bbox=dict(facecolor='none', edgecolor='green', boxstyle='round,pad=0.5', linewidth=1))
+        
+    return timeTextElems, riskTextElems, lines
 
-plots = [pdfPlot, quantilePlot, greenRedPlot]
-random.shuffle(plots)
+plots = [quantilePlot, pdfPlot, greenRedPlot]
+# plots = [quantilePlot]
+# random.shuffle(plots)
 for plot in plots:
-    fig, axs = plt.subplots(N_PLOTS, 1, figsize=(12, 7))
+    fig, axs = plt.subplots(N_PLOTS, 1, figsize=(3.5, 6))
     plot()
-    transformAxs(axs)
+    timeTextElems, riskTextElems, lines = transformAxs(axs)
 
     # add the sliders
     fig.subplots_adjust(bottom=0.25)
     fig.subplots_adjust(top=0.85)
     fig.subplots_adjust(hspace=1)
 
-    axrisk = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+    axrisk = fig.add_axes([0.35, 0.1, 0.5, 0.03])
     freq_slider = Slider(
         ax=axrisk,
         label='Risk slider (%)',
@@ -102,7 +96,7 @@ for plot in plots:
         valinit=50,
     )
 
-    axtime = fig.add_axes([0.25, 0.05, 0.65, 0.03])
+    axtime = fig.add_axes([0.35, 0.05, 0.5, 0.03])
     time_slider = Slider(
         ax=axtime,
         label='Time slider (min)',
@@ -110,6 +104,9 @@ for plot in plots:
         valmax=N_MINUTES,
         valinit=N_MINUTES//2,
     )
+
+    freq_slider.label.set_size(9)
+    time_slider.label.set_size(9)
 
     def updateFreq(val):
         times = [round(np.quantile(d, val/100), 1) for d in data]
@@ -121,7 +118,7 @@ for plot in plots:
         
         freq_slider.poly.set_fc('#2074b4')
         time_slider.poly.set_fc('grey')
-
+        
         fig.canvas.draw_idle()
         time_slider.reset()  
 
@@ -142,9 +139,6 @@ for plot in plots:
     # register the update function with each slider
     freq_slider.on_changed(updateFreq)
     time_slider.on_changed(updateTime)
-
-    manager = plt.get_current_fig_manager()
-    manager.full_screen_toggle()
     plt.show()
 
 
